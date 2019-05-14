131a7071b1d656fe599cc02a84a38dff9ca073a8
udarian <udarian@users.noreply.github.com>
2019-05-14 22:03:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+// check if the url is curled or visited normally
+if(preg_match("/curl|libcurl/", $_SERVER['HTTP_USER_AGENT'])){
+   
+   // if called, no styling
+   echo $_SERVER["REMOTE_ADDR"];
+   echo "\r\n";
+
+}
+else{
+
+   // if not curled, make it pretty
+   echo '<pre>';
+   echo $_SERVER["REMOTE_ADDR"];
+   echo '</pre>';
+   echo "\r\n";
+
+}
+
+?>
